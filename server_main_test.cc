63405b919f74a058a6404ffcec6fc7e3fcbc1f25
Chang Liu <liuchang2872@ucla.edu>
2017-02-01 18:56:52 -0800

    remove redundent files and modify server_main_test.cc

--- server_main_test.cc
+++ server_main_test.cc
@@ -1,52 +1,14 @@
 #include "gtest/gtest.h"
-#include "request_handler.h"
+#include "server_main.cc"
 
-static int getPort (const NginxConfig &config) {
-	for(const auto& statement : config.statements_) {
-		bool kl = true;
-		for (const std::string& token : statement->tokens_){
-			if(kl){
-				try { return stoi(token);} catch (...) {}
-			}
-			kl = (token != "listen");
-		}
-	}
-	return -1;
-}
 
-static std::string getBasePath(const NginxConfig& config) {
-	std::string pathfinder = "/";
-	for(const auto& statement : config.statements_){
-		bool kl = false;
-		for(const std::string& token : statement->tokens_){
-			kl = (token.find(pathfinder) != std::string::npos);
-			if(kl){
-				try { return token; } catch (...) {}
-			}
-		}
-	}
-	std::string s = "No valid path!";
-	return s;
-}
-
-class NginxStringConfigTest : public ::testing::Test {
-protected:
-	NginxConfigParser parser_;
-	NginxConfig config_;
-	bool ParseString(const std::string& config_string) {
-		std::stringstream config_stream(config_string);
-		return parser_.Parse(&config_stream, &config_);
-	}
+class NginxConfigParserTest : public ::testing::Test {
+  protected:
+    NginxConfigParser parser;
+    NginxConfig out_config;
 };
 
-TEST(NginxConfigParserTest, ExampleConfig){
-	NginxConfigParser parser;
-	NginxConfig out_config;
 
-	bool success = parser.Parse("example_config", &out_config);
-
-	EXPECT_TRUE(success) <<"example_config not parsed successfully";
-}
 
 TEST(NginxServerTest, GetPort){
 	NginxConfigParser parser;
@@ -63,67 +25,24 @@ TEST(NginxServerTest, GetPort){
 	EXPECT_TRUE(success) << "getPort function does not parse out port properly";
 }
 
-TEST(NginxConfigParserTest, MediaWikiConfig){
-	NginxConfigParser parser;
-	NginxConfig out_config;
-
-	bool success = parser.Parse("example_config", &out_config);
-
-	EXPECT_TRUE (success) << "MediaWikiConfig file did not parse successfully";
-}
 
 //TEST(NginxConfigParserTest, )
 
-TEST_F(NginxStringConfigTest, CurlyBracesConfig){
-	bool success = ParseString("foo { bar;}");
-	EXPECT_TRUE(success) << "Error with curly braces";
-}
 
-TEST_F(NginxStringConfigTest, SimpleConfig){
-	bool success = ParseString ("foo bar;");
-	EXPECT_TRUE(success) <<"one-line statement didn't parse";
-}
-
-TEST(NginxConfigParserTest, ParserConfig){
-	NginxConfigParser parser;
-	NginxConfig out_config;
-
-	bool success = parser.Parse("example_config", &out_config);
-
-	EXPECT_TRUE(success) << "config_parse file didn't parse successfully";
-}
-
-TEST(NginxServerTest, GetBasePath){
-	NginxConfigParser parser;
-	NginxConfig out_config;
-	parser.Parse("example_config", &out_config);
-	std::string base_path = getBasePath(out_config);
-	bool success;
-	if (base_path == "home/user/Team12"){
+TEST_F(NginxConfigParserTest, NestedGetPort){
+  std::string config_string = "server { listen 8070 ; }";
+  std::stringstream config_stream(config_string);
+  EXPECT_FALSE(parser.Parse(&config_stream, &out_config));
+  int port = getPort(out_config);
+  if(port == 8070)
+  {
 		success = true;
-	}else{
-		success = false;
-	}
-
-	EXPECT_TRUE(success) << "getBasePath function is not working properly";
-}
-
-TEST(NginxServerTest, EchoResponse){
-  HttpResponse rep;
-  HttpRequest req = {};
-  bool success;
-  std::string base_path = "/home/user/Team12";
-  EchoHandler echoHand;
-  std::map<std::string, std::string> handlerMap;
-  handlerMap["directory"] = base_path;
-  echoHand.EchoHandler::Init(handlerMap);
-  echoHand.EchoHandler::HandleRequest(req, &rep);
-
-  if (rep.headers_[1].second == "text/plain"){
-    success = true;
-  } else {
-    success = false;
   }
-
-  EXPECT_TRUE(success) << "Does not recognize echo server";
+  else
+  {
+		success == false;
+  }
+  EXPECT_TRUE(success) << "getPort function does not parse out port properly";
 }
+
+
